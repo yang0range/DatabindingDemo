98d4e8adedc08fb692c0e2e6a2b7c002724264fc
yangzc <yangzcorange@163.com>
2019-05-10 17:35:19 +0800

    简单属性

---
+++ app/src/main/java/com/yang/databindingdemo/model/Student.kt
@@ -0,0 +1,9 @@
+package com.yang.databindingdemo.model
+
+/**
+ * @author yangzc
+ *	@data 2019/5/10 17:07
+ *	@desc
+ *
+ */
+data class Student(val Name: String, val Age: Int)
---
+++ app/src/main/java/com/yang/databindingdemo/model2/Student.kt
@@ -0,0 +1,9 @@
+package com.yang.databindingdemo.model2
+
+/**
+ * @author yangzc
+ *	@data 2019/5/10 17:07
+ *	@desc
+ *
+ */
+data class Student(val Name: String, val Age: Int)
